a537a7c24e2428b066cd3cdd49503f00554b190d
HydraOrc <hydra0@bigmir.net>
2014-05-26 16:15:25 +0300

    added function declarations

--- test/express-middlware-router_test.js
+++ test/express-middlware-router_test.js
@@ -15,3 +15,67 @@ describe('expressMiddlwareRouter should have #awesome as function.', function() 
 		expressMiddlwareRouter.awesome().should.be.exactly("awesome")
 	});
 });
+
+describe('expressMiddlwareRouter should have #add_route as function.', function() {
+	it('should have #add_route', function() {
+		expressMiddlwareRouter.should.be.have.property('add_route');
+	});
+	
+	it('#add_route should have be a function', function() {
+		expressMiddlwareRouter.add_route.should.be.a.Function;
+	});
+});
+
+describe('expressMiddlwareRouter should have #get_route as function.', function() {
+	it('should have #get_route', function() {
+		expressMiddlwareRouter.should.be.have.property('get_route');
+	});
+	
+	it('#get_route should have be a function', function() {
+		expressMiddlwareRouter.get_route.should.be.a.Function;
+	});
+});
+
+describe('expressMiddlwareRouter should have #delete_route as function.', function() {
+	it('should have #delete_route', function() {
+		expressMiddlwareRouter.should.be.have.property('delete_route');
+	});
+	
+	it('#delete_route should have be a function', function() {
+		expressMiddlwareRouter.delete_route.should.be.a.Function;
+	});
+});
+
+describe('expressMiddlwareRouter should have #get_route_list as function.', function() {
+	it('should have #get_route_list', function() {
+		expressMiddlwareRouter.should.be.have.property('get_route_list');
+	});
+	
+	it('#get_route_list should have be a function', function() {
+		expressMiddlwareRouter.get_route_list.should.be.a.Function;
+	});
+	
+	it('#get_route_list should return an array', function() {
+		expressMiddlwareRouter.get_route_list().should.be.an.Array;
+	});
+});
+
+describe('expressMiddlwareRouter should have #update_route as function.', function() {
+	it('should have #update_route', function() {
+		expressMiddlwareRouter.should.be.have.property('update_route');
+	});
+	
+	it('#update_route should have be a function', function() {
+		expressMiddlwareRouter.update_route.should.be.a.Function;
+	});
+});
+
+describe('expressMiddlwareRouter should have #init_route as function.', function() {
+	it('should have #init_route', function() {
+		expressMiddlwareRouter.should.be.have.property('init_route');
+	});
+	
+	it('#init_route should have be a function', function() {
+		expressMiddlwareRouter.init_route.should.be.a.Function;
+	});
+});
